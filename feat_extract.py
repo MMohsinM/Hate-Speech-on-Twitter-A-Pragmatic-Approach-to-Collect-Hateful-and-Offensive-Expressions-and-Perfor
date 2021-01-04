c24614f05a6295b8338477d383ad8a4818ae71e1
MMohsinM <mohsin.muazzam@gmail.com>
2021-01-04 07:21:58 +0500

    These are the main functions used in project

--- feat_extract.py
+++ feat_extract.py
@@ -2,15 +2,54 @@ from nltk import FreqDist
 from nltk.util import ngrams
 from nltk.lm import NgramCounter
 from preprocess import get_attr
-import copy
+from copy import deepcopy
+from preprocess import Tweet, time_it
 
 
 # These are parts of speech tags corresponding to the noun, verb or (adjective or adverb)
-ptb_tags = ['NN', 'NNS', 'NNP', 'NNPS', 'VB', 'VBD', 'VBG', 'VBN', 'VBP', 'VBZ',
+utb_tags = ['NN', 'NNS', 'NNP', 'NNPS', 'VB', 'VBD', 'VBG', 'VBN', 'VBP', 'VBZ',
                 'JJ', 'JJR', 'JJS', 'RB', 'RBR', 'RBS']
 
+ptb_tags ={}
+ptb_tags['NN'] = "NOUN"
+ptb_tags['NNS'] = "NOUN"
+ptb_tags['NNP'] = "NOUN"
+ptb_tags['NNPS'] = "NOUN"
+ptb_tags['VB'] = "VERB"
+ptb_tags['VBD'] = "VERB"
+ptb_tags['VBG'] = "VERB"
+ptb_tags['VBN'] = "VERB"
+ptb_tags['VBP'] = "VERB"
+ptb_tags['VBZ'] = "VERB"
+ptb_tags['JJ'] = 'ADJECTIVE'
+ptb_tags['JJR'] = 'ADJECTIVE'
+ptb_tags['JJS'] = 'ADJECTIVE'
+ptb_tags['RB'] = 'ADVERB'
+ptb_tags['RBR'] = 'ADVERB'
+ptb_tags['RBS'] = 'ADVERB'
+ptb_tags['CC'] = 'COORDCONJUNCTION'
+ptb_tags['CD'] = 'CARDINAL'
+ptb_tags['DT'] = 'DETERMINER'
+ptb_tags['EX'] = 'EXISTTHERE'
+ptb_tags['FW'] = 'FOREIGNWORD'
+ptb_tags['IN'] = 'PREPOSITION'
+ptb_tags['LS'] = 'LISTMAKER'
+ptb_tags['MD'] = 'MODAL'
+ptb_tags['PDT'] = 'PREDETERMINER'
+ptb_tags['POS'] = 'POSSESSIVEEND'
+ptb_tags['PRP'] = 'PRONOUN'
+ptb_tags['PRP$'] = 'PRONOUN'
+ptb_tags['RP'] = 'PARTICLE'
+ptb_tags['SYM'] = 'SYMBOL'
+ptb_tags['TO'] = 'TO'
+ptb_tags['UH'] = 'INTERJECTION'
+ptb_tags['WDT'] = 'WHDETERMINER'
+ptb_tags['WP'] = 'WHDETERMINER'
+ptb_tags['WP$'] = 'WHDETERMINER'
+ptb_tags['WRB'] = 'WHDETERMINER'
+ 
 
-def delete_keys(num,class_dict):
+def delete_keys(num,class_dict, ratio_threshold=1.4):
     '''
     Implements the unigram ratio logic from
     the paper.
@@ -25,9 +64,69 @@ def delete_keys(num,class_dict):
         if deno != num:
             commons = set(class_dict[num]).intersection(set(class_dict[deno]))
             for common in commons:
-                if (class_dict[num][common]/class_dict[deno][common])<1.4:
+                if (class_dict[num][common]/class_dict[deno][common])<ratio_threshold:
                     delete_num.append(common)
     return set(delete_num)
+
+
+def attr_append_deepcopy(objs,attr1,attr2,cast):
+    all_attr1 = list(get_attr(objs,attr1))
+    all_attr2 = list(get_attr(objs,attr2))
+    all_attr1 = deepcopy(all_attr1)
+    for i in range(len(all_attr1)):
+        for j in range(len(all_attr1[i])):
+            all_attr1[i][j].append(all_attr2[i])
+            all_attr1[i][j] = cast(all_attr1[i][j])
+    return all_attr1
+
+
+def frequency(all_pos):
+    unigrams = [ngrams(pos,1) for pos in all_pos] # makes unigrams from pos
+    counter = NgramCounter(unigrams) # initializes the counter object
+    freq = counter.unigrams # gets the dict with pos as keys and frequencies as values
+    return freq
+
+
+def min_occ(freq, threshold):
+    thres_freq = {}  # thres_freq keeps those freqs which are greater or equal to 9
+    for key in freq.keys():
+        if freq[key] >= threshold: 
+            thres_freq[key]= freq[key]
+    return thres_freq
+
+
+def class_list(thres_freq, tags):
+    hate = {}
+    offn = {}
+    clean = {}
+    for key in thres_freq:
+        if key[1] in tags:
+            if key[2] =='clean':
+                clean[key[0]] = thres_freq[key]
+            elif key[2] =='offn':
+                offn[key[0]] = thres_freq[key]
+            elif key[2] =='hate':
+                hate[key[0]] = thres_freq[key]
+    return {'hate':hate,'offn':offn,'clean':clean}
+
+
+def pop_class_dict(class_dict):
+    delete_dict = {}
+    for key in class_dict:
+        delete_dict[key] = delete_keys(key,class_dict)
+    # delete_dict = {'hate':delete_keys('hate',class_dict),
+    #                 'offn':delete_keys('offn',class_dict),
+    #                 'clean': delete_keys('clean',class_dict)}
+    for dict in delete_dict:
+        for key in delete_dict[dict]:
+            class_dict[dict].pop(key)
+
+
+def features(class_dict):
+    features = []
+    for key in class_dict:
+        features += class_dict[key].keys()
+    return features
 
 
 def unigram_feat(tweets):
@@ -36,90 +135,76 @@ def unigram_feat(tweets):
     Calculate class ratios. This implements unigram feature
     extraction from the paper.
     '''
-    all_pos = get_attr(tweets,'pos')
-    classes = list(get_attr(tweets,'label'))
-    all_pos1 = copy.deepcopy(list(all_pos))
-    for i in range(len(all_pos1)):
-        for j in range(len(all_pos1[i])):
-            all_pos1[i][j].append(classes[i])
-            all_pos1[i][j] = tuple(all_pos1[i][j])
-    unigrams = [ngrams(pos,1) for pos in all_pos1] # makes unigrams from pos
-    counter = NgramCounter(unigrams) # initializes the counter object
-    freq = counter.unigrams # gets the dict with pos as keys and frequencies as values
-    thres_freq = {}  # thres_freq keeps those freqs which are greater or equal to 9
-    for key in freq.keys():
-        if freq[key] >= 9: 
-            thres_freq[key]= freq[key]
-    # Rewrite this part because class is actually hate, offensive and clean
-    hate = {}
-    offn = {}
-    clean = {}
-    for key in thres_freq:
-        if key[1] in ptb_tags:
-            if key[2] =='clean':
-                clean[key[0]] = thres_freq[key]
-            elif key[2] =='offn':
-                offn[key[0]] = thres_freq[key]
-            elif key[2] =='hate':
-                hate[key[0]] = thres_freq[key]
-    class_dict = {'hate':hate,'offn':offn,'clean':clean}
-    delete_dict = {'hate':delete_keys('hate',class_dict),
-                    'offn':delete_keys('offn',class_dict),
-                    'clean': delete_keys('clean',class_dict)}
-    for dict in delete_dict:
-        for key in delete_dict[dict]:
-            class_dict[dict].pop(key)
-    unigram = []
-    for key in class_dict:
-        unigram += class_dict[key].keys()
-    return unigram
+    all_pos = attr_append_deepcopy(tweets, 'pos', 'label', tuple)
+    freq = frequency(all_pos)
+    thres_freq = min_occ(freq, threshold=9)
+    class_dict = class_list(thres_freq, utb_tags)
+    pop_class_dict(class_dict)
+    return features(class_dict)
 
+@time_it
+def simplified_tag(all_pos):
+    new_tags = []
+    for pos in all_pos:
+        sent = []
+        for list_ in pos:
+            if list_[1] in utb_tags:
+                #score = Tweet.senti.getSentiment(list_[0])[0]
+                score = Tweet.word_score(Tweet.sid.polarity_scores(list_[0]))
+                if score>=0:
+                    sentiment = "Positive_"
+                else:
+                    sentiment = 'Negative_'
+            else:
+                sentiment=""
+            if list_[1] in ptb_tags:
+                sent.append(sentiment+ptb_tags[list_[1]])
+            else:
+                sent.append(sentiment+".")
+        new_tags.append((tuple(sent), list_[-1]))
+    return new_tags
+
+
+def get_all_pattern(all_tags, L=5):
+    all_patterns = []
+    for tags in all_tags:
+        tags_sent = tags[0]
+        patterns = tuple((tuple(tags_sent[i:i+L]),tags[1]) for i in range(len(tags_sent)-L+1))
+        all_patterns += patterns
+    return all_patterns
 
 
 def pattern_feat(tweets):
-    '''
-    Gets all the unigrams, counts frequency and keeps > 9
-    Calculate class ratios. This implements unigram feature
-    extraction from the paper.
-    '''
-    all_pos = get_attr(tweets,'pos')
-    classes = list(get_attr(tweets,'label'))
-    all_pos1 = copy.deepcopy(list(all_pos))
-    for i in range(len(all_pos1)):
-        for j in range(len(all_pos1[i])):
-            all_pos1[i][j].append(classes[i])
-            all_pos1[i][j] = tuple(all_pos1[i][j])
-    unigrams = [ngrams(pos,1) for pos in all_pos1] # makes unigrams from pos
-    counter = NgramCounter(unigrams) # initializes the counter object
-    freq = counter.unigrams # gets the dict with pos as keys and frequencies as values
-    thres_freq = {}  # thres_freq keeps those freqs which are greater or equal to 9
-    for key in freq.keys():
-        if freq[key] >= 9: 
-            thres_freq[key]= freq[key]
-    # Rewrite this part because class is actually hate, offensive and clean
-    hate = {}
-    offn = {}
-    clean = {}
+    tweets_gte_5 = [tweet for tweet in tweets if len(tweet.tokens)>=5]
+    all_pos = attr_append_deepcopy(tweets_gte_5, 'pos', 'label', tuple)
+    new_tags = simplified_tag(all_pos)
+    all_patterns = get_all_pattern(new_tags, L=5)
+    hate=[]
+    offn=[]
+    clean=[]
+    class_list = {'hate':hate,'offn':offn,'clean':clean}
+    for patterns in all_patterns:
+        class_list[patterns[1]].append(patterns[0])
+    freq = {}
+    for key in class_list:
+        freq[key] = FreqDist(class_list[key])
+    thres_freq = {}
+    for key in freq:
+        thres_freq[key] = min_occ(freq[key], threshold=7)
+    delete_dict = {}
     for key in thres_freq:
-        if key[1] in ptb_tags:
-            if key[2] =='clean':
-                clean[key[0]] = thres_freq[key]
-            elif key[2] =='offn':
-                offn[key[0]] = thres_freq[key]
-            elif key[2] =='hate':
-                hate[key[0]] = thres_freq[key]
-    class_dict = {'hate':hate,'offn':offn,'clean':clean}
-    delete_dict = {'hate':delete_keys('hate',class_dict),
-                    'offn':delete_keys('offn',class_dict),
-                    'clean': delete_keys('clean',class_dict)}
+        delete_dict[key] = delete_keys(key, thres_freq)
     for dict in delete_dict:
         for key in delete_dict[dict]:
-            class_dict[dict].pop(key)
-    unigram = []
-    for key in class_dict:
-        unigram += class_dict[key].keys()
-    return unigram
+            thres_freq[dict].pop(key)
+    return features(thres_freq)
 
 
 # all_pos = get_attr(tweets,'pos')
 # unigram_feat(tweets)
+# class_=[]
+# masla = []
+# for pos in all_pos:
+#     for tup in pos:
+#         if len(tup)>3:
+#             masla.append([all_pos.index(pos),pos.index(tup)])
--- preprocess.py
+++ preprocess.py
@@ -1,20 +1,25 @@
 import re
 import os
+import time
 import subprocess as sbp
-from nltk import download  as n_download
+#from concurrent.futures import ProcessPoolExecutor
+import numpy as np
+import pandas as pd
+from tqdm import tqdm
+from nltk import download  as n_download, FreqDist
 from nltk.stem import WordNetLemmatizer 
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
 from nltk.tokenize import TweetTokenizer
-from nltk.util import ngrams
-import pandas as pd
 from split_hashtags import split_hashtag_to_words_all_possibilities as hd
 from sentistrength import PySentiStr
-import numpy as np
-from tqdm import tqdm
+
+# try:
+#     import cPickle as pickle
+# except ImportError: 
+#     import pickle
 
 from functools import wraps
-import time
-import os
+
 def time_it(func):
     @wraps(func)
     def wrapper(*args, **kwargs):
@@ -43,6 +48,7 @@ data_files =  os.path.abspath("datasets\\hate_offensive_2.csv")
 class Tweet:
     #Class varibles
     pos_input_path = 'tweet_token_input.txt'
+    pos_batch_path = 'tweet_token_batch.txt'
     pos_output_path = 'tweet_pos_output.txt'
     contrast_words = ('but','however','in contrast','yet','differ','difference',
                     'variation', 'still','on the contrary','conversely','otherwise',
@@ -61,6 +67,50 @@ class Tweet:
     # Note: Provide absolute path instead of relative path
     senti.setSentiStrengthPath('C:\\java_modules\\twitie-tagger\\SentiStrength.jar') 
     senti.setSentiStrengthLanguageFolderPath('C:\\java_modules\\SentiStrength_Data')
+    unigrams = np.load('unigrams.npy')
+    utb_tags = ['NN', 'NNS', 'NNP', 'NNPS', 'VB', 'VBD', 'VBG', 'VBN', 'VBP', 'VBZ',
+                'JJ', 'JJR', 'JJS', 'RB', 'RBR', 'RBS']
+
+    patterns = np.load('patterns.npy')
+    
+    ptb_tags ={}
+    ptb_tags['NN'] = "NOUN"
+    ptb_tags['NNS'] = "NOUN"
+    ptb_tags['NNP'] = "NOUN"
+    ptb_tags['NNPS'] = "NOUN"
+    ptb_tags['VB'] = "VERB"
+    ptb_tags['VBD'] = "VERB"
+    ptb_tags['VBG'] = "VERB"
+    ptb_tags['VBN'] = "VERB"
+    ptb_tags['VBP'] = "VERB"
+    ptb_tags['VBZ'] = "VERB"
+    ptb_tags['JJ'] = 'ADJECTIVE'
+    ptb_tags['JJR'] = 'ADJECTIVE'
+    ptb_tags['JJS'] = 'ADJECTIVE'
+    ptb_tags['RB'] = 'ADVERB'
+    ptb_tags['RBR'] = 'ADVERB'
+    ptb_tags['RBS'] = 'ADVERB'
+    ptb_tags['CC'] = 'COORDCONJUNCTION'
+    ptb_tags['CD'] = 'CARDINAL'
+    ptb_tags['DT'] = 'DETERMINER'
+    ptb_tags['EX'] = 'EXISTTHERE'
+    ptb_tags['FW'] = 'FOREIGNWORD'
+    ptb_tags['IN'] = 'PREPOSITION'
+    ptb_tags['LS'] = 'LISTMAKER'
+    ptb_tags['MD'] = 'MODAL'
+    ptb_tags['PDT'] = 'PREDETERMINER'
+    ptb_tags['POS'] = 'POSSESSIVEEND'
+    ptb_tags['PRP'] = 'PRONOUN'
+    ptb_tags['PRP$'] = 'PRONOUN'
+    ptb_tags['RP'] = 'PARTICLE'
+    ptb_tags['SYM'] = 'SYMBOL'
+    ptb_tags['TO'] = 'TO'
+    ptb_tags['UH'] = 'INTERJECTION'
+    ptb_tags['WDT'] = 'WHDETERMINER'
+    ptb_tags['WP'] = 'WHDETERMINER'
+    ptb_tags['WP$'] = 'WHDETERMINER'
+    ptb_tags['WRB'] = 'WHDETERMINER'
+
     try:
         tk = TweetTokenizer()
     except LookupError:
@@ -77,6 +127,15 @@ class Tweet:
         n_download('vader_lexicon')
         sid = SentimentIntensityAnalyzer()
 
+    # with open('Emoji_Dict.p', 'rb') as fp:
+    #     Emoji_Dict = pickle.load(fp)
+    # Emoji_Dict = {v: k for k, v in Emoji_Dict.items()}
+
+    # def convert_emojis_to_word(text):
+    #     for emot in Tweet.Emoji_Dict:
+    #         text = re.sub(r'('+emot+')', " ".join(Tweet.Emoji_Dict[emot].replace(",","").replace(":","").split()), text)
+    #     return text
+
     # static_method
     def word_score(scores):
         """
@@ -92,13 +151,15 @@ class Tweet:
         else:
             return -1
 
-    
+
+
     def __init__(self, text_string, label):
         self.raw = text_string
         self.label = label
         self.tidy = self.preprocess()
         self.tokens = self.basic_tokenize()
-        self.pos = None #self.POS_gate_java() #This is a time consuming 
+        self._pos = None
+        #self.pos = None #self.POS_gate_java() #This is a time consuming 
         # function so at initiallizing its set None
         # Manually set it by calling POS_gate_java or
         # set it in a batch
@@ -111,7 +172,7 @@ class Tweet:
             Accepts a text string and replaces:
             1) urls with nothing
             2) lots of whitespace with one instance
-            3) mentions with nothing
+            3) mentions (@) with nothing
 
             This allows us to get standardized counts of urls and mentions
             Without caring about specific people mentioned
@@ -123,9 +184,11 @@ class Tweet:
             hashtag_regex  = r'#[\w\-]+'
             end_space_regex = r'\s$'
             parsed_text = re.sub(giant_url_regex, '', self.raw)
+            #parsed_text = Tweet.convert_emojis_to_word(parsed_text)
             parsed_text = re.sub(mention_regex, '', parsed_text)
             parsed_text = re.sub(hashtag_regex, '', parsed_text)
             parsed_text = re.sub(space_pattern, ' ', parsed_text)
+            
             parsed_text = re.sub(end_space_regex, '', parsed_text)
             self.tidy = parsed_text
             return parsed_text
@@ -140,7 +203,22 @@ class Tweet:
 
     def token_to_file(self):
         with open(self.pos_input_path, mode='w+') as f:
-            f.write(" ".join(self.tokens))
+            if len(self.tokens)==0:
+                addr = "."
+            else:
+                addr = ""
+            f.write(" ".join(self.tokens)+addr)
+            
+    @property
+    def pos(self):
+        return self._pos
+
+    @pos.setter
+    def pos(self,pos_tags):
+        for pos_ in pos_tags:
+            if len(pos_)>2:
+                pos_tags[pos_tags.index(pos_)] = [pos_[0],pos_[-1]]
+        self._pos = pos_tags
 
 
     def POS_gate_java(self):
@@ -149,8 +227,36 @@ class Tweet:
                             "models/gate-EN-twitter.model",
                             self.pos_input_path])
         x = x.decode().split('\n')[:-1]
-        self.pos = [tuple(i.split("_")) for i in x[0].strip().split(" ")]
+        self.pos = [i.split("_") for i in x[0].strip().split(" ")]
         return self.pos
+
+
+    def simplified_tag(self):
+        sent = []
+        for list_ in self.pos:
+            if list_[1] in Tweet.utb_tags:
+                #score = senti_analyze.getSentiment(list_[0])[0]
+                score = Tweet.word_score(Tweet.sid.polarity_scores(list_[0]))
+                if score>=0:
+                    sentiment = "Positive_"
+                else:
+                    sentiment = 'Negative_'
+            else:
+                sentiment=""
+            if list_[1] in Tweet.ptb_tags:
+                sent.append(sentiment+Tweet.ptb_tags[list_[1]])
+            else:
+                sent.append(sentiment+".")
+        return sent
+
+
+    def generate_patterns(self,L=5):
+        if len(self.tokens)<5:
+            return None
+        else:
+            tags_sent = self.simplified_tag()
+            patterns = [tags_sent[i:i+L] for i in range(len(tags_sent)-L+1)]
+            return patterns
 
 
     def lemmatization(self):
@@ -206,13 +312,12 @@ class Tweet:
         '''
         hash_tokens = []
         for hashtag in self.get_hashtags():
-            decomp = hd(hashtag)[0]
+            decomp = hd(hashtag)
             if len(decomp)==0: #If no hashtag is returned
                 hash_tokens.append(hashtag)
             else: #If 
-                hash_tokens.append(decomp) 
+                hash_tokens.append(decomp[0]) 
         return hash_tokens
-
 
     def semantic_features(self):
         """
@@ -232,12 +337,12 @@ class Tweet:
         neg_slangs = 0 # number of negative slangs
         pos_hash = 0
         neg_hash = 0
-        word_polar = [] # Contains the polarity of each word
-        scores = []
         PW = 0
         NW = 0
+        #senti_analyze = Tweet.senti.getSentiment
         for word in self.tokens:
-            score = Tweet.senti.getSentiment(word)[0]
+            #score = senti_analyze(word)[0]
+            score = Tweet.word_score(Tweet.sid.polarity_scores(word))
             if score>0:
                 if word in Tweet.slangs.word:
                     pos_slangs+=1
@@ -263,6 +368,7 @@ class Tweet:
                 neg_hash += 1
         return (PW, NW, ratio, pos_slangs, neg_slangs, pos_hash, neg_hash)
                 
+
     # Dont call it before setting the POS
     def syntatic_features(self):
         '''
@@ -287,25 +393,67 @@ class Tweet:
                 inter_count += 1
         return (tidy.count("!"), tidy.count("?"), tidy.count("."), capital_count, 
                 int(tidy.count("'")/2), inter_count, word_count)
+
+    def unigram_feature(self):
+        '''
+        Takes a tweet and returns the unigram feature vector
+        '''
+        x = Tweet.unigrams
+        feat_vec = np.full((len(x)), False, dtype=np.bool)
+        for token in self.tokens:
+            try:
+                feat_vec[np.where(x == token)]=True # Loop over tokens and try to match it in unigrams
+            except ValueError: # if token is not in the list
+                continue
+        return feat_vec
     
+    def pattern_feature(self):
+        x = Tweet.patterns
+        feat_vec = np.full((len(x)), False, dtype=np.bool)
+        if len(self.tokens)>5:
+            for pattern in self.generate_patterns():
+                try:
+                    feat_vec[np.where((x == pattern).all(axis=1))]=True # Loop over tokens and try to match it in unigrams
+                except ValueError: #Pattern not matched
+                    continue
+        return feat_vec
+            
+    def get_feature_vector(self):
+        return np.hstack((self.semantic_features(), self.syntatic_features(), 
+                            self.unigram_feature(), self.pattern_feature()))
+        
+
 #V_Tweet = np.vectorize(Tweet)
 
 #encoding=encode)
 def batch_to_file(tweets, encode='utf_8'):
-    all_tokens = []
-    for tweet in tweets:
-        all_tokens.append(tweet.tokens)
+    '''
+    This is when a input batch is available
+    Then this function is used to create the twitie_tag.jar
+    input file.
+    '''
     #df = pd.DataFrame(all_tokens)
     #df.to_csv(Tweet.pos_input_path,sep=' ', index=False)
-    with open(tweets[0].pos_input_path, mode='w+' )as f:
+    with open(Tweet.pos_batch_path, mode='w+' )as f:
         for i in tqdm(range(tweets.shape[0])):
-            f.write(" ".join(tweets[i].tokens)+"\n")
+            tks = tweets[i].tokens
+            if i != (tweets.shape[0]-1):
+                addr = "\n"
+            else:
+                addr = ''
+            if len(tks)>0:
+                f.write(" ".join(tks)+addr)
+            else:
+                f.write(".\n")
         #for tweet in tweets:
         #    f.write(" ".join(tweet.tokens)+"\n")
 
 
-def POS_gate_batch(path_in,path_out):
+def POS_gate_batch():
     '''
+    This calls the java POS tagger which accepts
+    accepts the token input file and returns 
+    pos tags.
     #Args
     path_: file from which POS are written
             assumes that the required tokens
@@ -316,14 +464,14 @@ def POS_gate_batch(path_in,path_out):
     '''
     x = sbp.check_output(["java", "-jar","twitie_tag.jar",
                         "models/gate-EN-twitter.model",
-                        path_in])
-    x = x.decode().split('\n')[:-1]
+                        Tweet.pos_batch_path])
+    x = x.decode().split('\n')
     all_pos = [list(i.strip().split(" ")) for i in x]
-    # Loop converts the string sep by_ like man_NN
+    # Loop converts the string separated by _ like man_NN
     # into tuple (man,NN) to change into standard NLTK output
-    for sent in all_pos: # If time i will refactor it O(n^2)
+    for sent in all_pos: # If time remains I will refactor it. O(n^2)
         for i in range(len(sent)):
-            sent[i] = tuple(sent[i].split("_")) 
+            sent[i] = sent[i].split("_")
     return all_pos
 '''
 def POS_gate_batch(path_in,path_out):   
@@ -350,28 +498,43 @@ def POS_gate_batch(path_in,path_out):
 #         tweets[index] = Tweet(row['tweet'], row['class'])
 #     return tweets
 
-
 # @time_it
 # def init_tweet1(df):
 #     VTweet = np.vectorize(Tweet)
 #     return VTweet(df['tweet'], df['class'])
-    
+
+def pos_tokens(pos):
+    tokens = []
+    for tup in pos:
+        tokens.append(tup[0])
+    return tokens
+
+def match_pos(tweet,pos):
+    tokens_arr2 = pos_tokens(pos)
+    if tweet.tokens == tokens_arr2:
+        return True
+    else:
+        return False
+
 
 @time_it
-def list_of_tweets(df):
+def list_of_tweets(data):
     '''
     Returns a numpy array of tweets
     from a given DataFrame.
     '''
     VTweet = np.vectorize(Tweet)
-    tweets= VTweet(df['tweet'], df['class'])
+    tweets= VTweet(data['tweet'], data['class'])
     batch_to_file(tweets)
-    all_pos = POS_gate_batch(Tweet.pos_input_path,Tweet.pos_output_path)
-    for i in range(len(all_pos)):
+    # with ProcessPoolExecutor() as e:
+    #     future = e.submit(POS_gate_batch)
+    #     all_pos = future.result()
+    all_pos = POS_gate_batch()
+    for i in range(len(tweets)):
         tweets[i].pos = all_pos[i]
     return tweets
     
-@time_it
+
 def get_attr(arr, attr):
     ''' Creates a generator of attributes 
     from a given iter of objects
@@ -384,16 +547,25 @@ def get_attr(arr, attr):
     return (i.__getattribute__(attr) for i in arr)
         
 
-
-@time_it
-def unigram_features(tokens):
-    unigram = None
-    return unigram
-
-
 # import numpy as np
+# tweets = np.load('list_obj3.npy', allow_pickle=True)
 # import pandas as pd
-# from preprocess import list_of_tweets
-# data = pd.read_csv('datasets\\data1.csv')
-# x = list_of_tweets(data)
-# np.save("tweet_obj.npy",x)
+# from preprocess import (list_of_tweets, Tweet, POS_gate_batch, 
+#                           pos_tokens,match_pos, batch_to_file,
+#                           get_attr)                           
+# data = pd.read_csv('data1_3.csv')
+# tweets = list_of_tweets(data)
+# # np.save("LIST_TWEET.npy",tweets)
+# # np.save("list_obj3.npy",tweets)
+# # tweets = np.load('LIST_TWEET.npy', allow_pickle=True)
+# tweets = np.load('list_obj3.npy', allow_pickle=True)
+# all_pos = get_attr(tweets,'pos')
+# def list_match():
+#     last = 0
+#     for i in range(len(x)):
+#         if match_pos(tweets[i],x[i]):
+#             continue
+#         else:
+#             if last != i-1:
+#                 print(i)
+#             last = i
